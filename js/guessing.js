0a3a6829755384ffa7aa49bcd11f8f207708f412
logansportfolio <34245836+logansportfolio@users.noreply.github.com>
2017-12-04 11:36:41 -0600

    Update guessing.js

--- js/guessing.js
+++ js/guessing.js
@@ -1,1 +1,21 @@
-
+document.addEventListener("keypress", enter);
+function enter() {
+  var y = event.keyCode;
+  if (y === 13) {
+    return guess();
+  }
+  else {
+    return false;
+  }
+}
+function guess() {
+  var guess = document.getElementById("guess").value;
+  if (guess !== "pluto") {
+    document.getElementById("out").innerHTML = "That is not correct!";
+    document.getElementById("guess").value = "";
+  }
+  else {
+    document.getElementById("out").innerHTML = "That is correct!";
+  }
+  document.getElementById("guess").focus();
+}  
